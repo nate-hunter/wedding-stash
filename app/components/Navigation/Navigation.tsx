14b3e328e8a2a323cf4911b152cf68e33a5b2eb3
Nate Hunter <nhunter@indemand.com>
2025-09-14 19:50:35 -0400

    Refactor Navigation component and styles for improved layout and user experience
    - Updated navigation styles to enhance responsiveness and visual hierarchy, including adjustments to padding, colors, and hover effects.
    - Introduced a new structure for navigation links, allowing for dynamic rendering based on the current path.
    - Enhanced user authentication display with conditional rendering for sign-in and sign-out options.
    - Improved accessibility and usability by refining the layout and incorporating user feedback.

--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -6,11 +6,41 @@ import { User } from '@supabase/supabase-js';
 
 import { createClient } from '@/utils/supabase/client';
 
-import Button from '@/components/Button';
+import './navigation.css';
+import { UploadIcon } from '../Icon/icons/UploadIcon';
+import { usePathname } from 'next/navigation';
+
+type NavLink = {
+  key: string;
+  label: string;
+  path: string;
+};
+
+const NAV_LINKS: Array<NavLink> = [
+  {
+    key: 'nav-home',
+    label: 'Home',
+    path: '/',
+  },
+
+  {
+    key: 'nav-galleries',
+    label: 'Galleries',
+    path: '/galleries',
+  },
+  {
+    key: 'nav-user-media-items',
+    label: 'My Collections',
+    path: '/collections',
+  },
+];
 
 export default function Navigation() {
-  const [user, setUser] = useState<User | null>(null);
   const supabase = createClient();
+
+  const pathname = usePathname();
+
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     const getUser = async () => {
@@ -19,9 +49,7 @@ export default function Navigation() {
       } = await supabase.auth.getUser();
       setUser(user);
     };
-
     getUser();
-
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
@@ -32,44 +60,48 @@ export default function Navigation() {
   }, [supabase.auth]);
 
   return (
-    <nav className='nav-bar'>
-      <div className='nav-container'>
-        <div className='inline-flex items-center gap-sp2'>
-          <Link href='/' className='nav-brand'>
-            Wedding Stash
-          </Link>
-          <Link href='/gallery' className='nav-link'>
-            Gallery
-          </Link>
-          {/* <Link href='/albums' className='nav-link'>
-            Albums
-          </Link> */}
-          {/* <Link href='/gallery/all' className='nav-link'>
-            All Media
-          </Link> */}
-        </div>
+    <nav className='nav-bar px-5 py-4 md:py-7 md:px-6 md:h-9'>
+      <div className='flex flex-col gap-4 align-items-center md:flex-row md:gap-7'>
+        {/* APP NAME / BRAND / LOGO*/}
+        <span className='nav-brand'>
+          Lisa & Nate
+          {/* Stash */}
+        </span>
 
-        <div className='nav-links'>
-          {user ? (
-            <>
-              {/* <Button href='/google-upload' asLink variant='sunset' width='fit'>
-                <UploadIcon size={15} />
-                Google Photos
-              </Button> */}
-              {/* AUTHENTICATION */}
-              <form action='/auth/signout' method='post' className='nav-form'>
-                <Button type='submit' bg='inverted' width='fit'>
-                  Sign Out
-                </Button>
-              </form>
-            </>
-          ) : (
-            <Link href='/login' className='nav-link'>
-              Sign In
+        {/* NAV LINKS */}
+        {NAV_LINKS.map(({ key, label, path }) => {
+          const isActive = pathname === path || (pathname.startsWith(path) && path !== '/');
+          return (
+            <Link key={key} href={path} className={`nav-link ${isActive ? 'nav-link-active' : ''}`}>
+              {label}
             </Link>
-          )}
-        </div>
+          );
+        })}
       </div>
+
+      {user ? (
+        <div className='flex flex-col-reverse gap-4 align-items-center md:flex-row md:gap-7'>
+          {/* Replace Link with a button to open a modal for uploading media items? */}
+          <Link href='/upload' className='btn btn-cta-outlined btn-sm'>
+            <UploadIcon size={16} />
+            Upload
+          </Link>
+
+          {/* AUTHENTICATION */}
+          <div className='flex align-items-center gap-6'>
+            <span className='nav-user text-xs md:text-base'>{user.email}</span>
+            <form action='/auth/signout' method='post' className='nav-form'>
+              <button type='submit' className='btn btn-inverted-outlined btn-sm w-max'>
+                <span>Sign Out</span>
+              </button>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <Link href='/login' className='nav-link'>
+          Sign In
+        </Link>
+      )}
     </nav>
   );
 }
