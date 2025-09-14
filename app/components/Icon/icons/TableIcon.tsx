466bb30528642d770662ea4448f7af60f93884ab
Nate Hunter <nhunter@indemand.com>
2025-09-14 19:51:21 -0400

    Add new icons: FilterBars, Filter, Grid, Maximize, Plus, Table, and update index export

---
+++ app/components/Icon/icons/TableIcon.tsx
@@ -0,0 +1,25 @@
+import { type IconProps } from '../+types';
+
+export const TableIcon = ({ size = 24, strokeWidth = 2, ...props }: Partial<IconProps>) => {
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      height={size}
+      width={size}
+      fill='none'
+      stroke='currentColor'
+      strokeWidth={strokeWidth}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <line x1='8' y1='6' x2='21' y2='6'></line>
+      <line x1='8' y1='12' x2='21' y2='12'></line>
+      <line x1='8' y1='18' x2='21' y2='18'></line>
+      <line x1='3' y1='6' x2='3.01' y2='6'></line>
+      <line x1='3' y1='12' x2='3.01' y2='12'></line>
+      <line x1='3' y1='18' x2='3.01' y2='18'></line>
+    </svg>
+  );
+};
