4176f670e871c76c0099f58b2db085ecbf1a249b
nate-hunter <hunter.j.nate@gmail.com>
2025-12-30 00:55:40 -0500

    Removed unused/error-causing component

--- app/components/UploadMediaItems/index.ts
+++ app/components/UploadMediaItems/index.ts
@@ -1,7 +1,6 @@
 export { default } from './UploadMediaItems';
 export { default as Dropzone } from './_components/Dropzone';
 export { default as UploadButton } from './_components/UploadButton';
-export { UploadItem } from './_components/MediaFileItem';
 export { FileList } from './_components/FileList';
 export { useUnifiedUpload } from './hooks/useUnifiedUpload';
 export type {
